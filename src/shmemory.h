9973a534b2a393ed4707e664289dad9df3d80fd3
Shane Bostock <shane.bostock@gmail.com>
2024-12-12 16:07:41 -0800

    removed remained of c++ impl

--- src/shmemory.h
+++ src/shmemory.h
@@ -6,6 +6,4 @@ int get_id_new(size_t shmem_size);
 int get_id(size_t shmem_size);
 void* create_shared_memory(size_t shmem_size);
 void* get_shared_memory(size_t shmem_size);
-
-
 #endif /* SHMEMORY_H */
